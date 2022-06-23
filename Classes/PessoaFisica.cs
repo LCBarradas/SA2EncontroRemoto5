e672af2dd7df3b3a75f8c9d6636d7c410c3065f8
Luiz Carlos Barradas <100002313+LCBarradas@users.noreply.github.com>
2022-06-23 17:33:38 -0300

    Add files via upload

---
+++ Classes/PessoaFisica.cs
@@ -0,0 +1,47 @@
+using BE5.Interfaces;
+
+namespace BE5.Classes
+{
+    public class PessoaFisica : Pessoa, IPessoaFisica
+    {
+
+        public string ?cpf { get; set; }
+        
+        public string ?dataNascimento { get; set; }
+        
+        public override float PagarImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarDataNascimento(DateTime dataNasc)
+        {
+            DateTime dataAtual = DateTime.Today;
+            double anos = (dataAtual - dataNasc).TotalDays / 365;
+            
+            if (anos >= 18){
+                return true;
+            }
+            else{
+                return false;
+            }
+             
+        }
+    
+        public bool ValidarDataNascimento(string dataNasc)
+        {
+            DateTime dataConvertida;
+            //verificar se a string está em um formato válido.
+            if (DateTime.TryParse(dataNasc, out dataConvertida)){// tryParse tenta converter e coloca na saida dataConvertida.
+                //Console.WriteLine($"{dataConvertida}");
+                DateTime dataAtual = DateTime.Today;
+                double anos = (dataAtual - dataConvertida).TotalDays / 365;
+                if (anos >= 18){
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }      
+     }
+}
